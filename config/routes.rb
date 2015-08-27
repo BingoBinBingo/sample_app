a75641eba053884661e83bbbc18a316cbd8b6db6
黄彬 <575395982@qq.com>
2015-08-27 22:35:09 +0800

    (1)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   
-  root             'static_pages#home'
-  get 'about'   => 'static_pages#about'
-  get 'help'    => 'static_pages#help'
-  get 'contact' => 'static_pages#contact'
-  get 'signup'  => 'users#new'
+  get 'sessions/new'
+
+  root               'static_pages#home'
+  get   'about'   => 'static_pages#about'
+  get   'help'    => 'static_pages#help'
+  get   'contact' => 'static_pages#contact'
+  get   'signup'  => 'users#new'
+  get   'login'   => 'sessions#new'
+  post  'login'   => 'sessions#create'
+	delete 'logout' => 'sessions#destroy'
  # post 'users'  => 'users#create'
   resources :users
 
